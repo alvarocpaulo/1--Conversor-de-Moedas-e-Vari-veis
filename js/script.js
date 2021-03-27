a326418ba5307c08ed82e88d275662cfc6f3e952
Álvaro Paulo <acptdb@gmail.com>
2021-03-27 18:20:59 -0300

    continue

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
 
-let valorDolar = prompt("Qual o valor em dolar da conversão?")
-parseFloat(valorDolar);
-
-let valorReal = valorDolar * 5.5;
-valorReal = valorReal.toFixed(2);
-alert("O valor em real é de: R$" + valorReal);
-
