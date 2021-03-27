4e7acc317c3582dc2d0fda9e77be266cd9ace28a
Álvaro Paulo <acptdb@gmail.com>
2021-03-27 18:49:16 -0300

    continue

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -1,1 +1,6 @@
 "use strict";
+
+var primeiroValor = parseInt(prompt("Digite o primeiro valor:"));
+var segundoValor = parseInt(prompt("Digite o segundo valor:"));
+soma = primeiroValor + segundoValor;
+document.write("<h1>" + primeiroValor + " + " + segundoValor + " = " + soma + "</h1>");
--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-
+let primeiroValor = parseInt(prompt("Digite o primeiro valor:"));
+let segundoValor = parseInt(prompt("Digite o segundo valor:"));
+soma = primeiroValor + segundoValor;
+document.write("<h1>"+ primeiroValor + " + " + segundoValor
++ " = " + soma + "</h1>");
