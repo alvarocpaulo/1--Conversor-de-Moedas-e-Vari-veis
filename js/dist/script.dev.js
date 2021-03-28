bbae4c4fbfcf3ca6bfe2d6e087621dcbdffc6d34
Álvaro Paulo <acptdb@gmail.com>
2021-03-28 17:14:22 -0300

    continue

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -1,6 +1,7 @@
 "use strict";
 
-var primeiroValor = parseInt(prompt("Digite o primeiro valor:"));
-var segundoValor = parseInt(prompt("Digite o segundo valor:"));
-soma = primeiroValor + segundoValor;
-document.write("<h1>" + primeiroValor + " + " + segundoValor + " = " + soma + "</h1>");
+//let operacao = parseFloat(prompt("Digite 1 para fazer uma divisão, 2 para multiplicar, 3 para soma, 4 para subtração:"))
+var primeiroValor = parseFloat(prompt("Digite o primeiro valor:"));
+var segundoValor = parseFloat(prompt("Digite o segundo valor:"));
+resultado = primeiroValor + segundoValor;
+document.write("<h1>" + primeiroValor.toFixed(2) + " + " + segundoValor.toFixed(2) + " = " + resultado + "</h1>");
--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-let primeiroValor = parseInt(prompt("Digite o primeiro valor:"));
-let segundoValor = parseInt(prompt("Digite o segundo valor:"));
-soma = primeiroValor + segundoValor;
-document.write("<h1>"+ primeiroValor + " + " + segundoValor
-+ " = " + soma + "</h1>");
+//let operacao = parseFloat(prompt("Digite 1 para fazer uma divisão, 2 para multiplicar, 3 para soma, 4 para subtração:"))
+let primeiroValor = parseFloat(prompt("Digite o primeiro valor:"));
+let segundoValor = parseFloat(prompt("Digite o segundo valor:"));
+resultado = primeiroValor + segundoValor;
+document.write("<h1>" + primeiroValor.toFixed(2) + " + " + segundoValor.toFixed(2)
+   + " = " + resultado + "</h1>");
