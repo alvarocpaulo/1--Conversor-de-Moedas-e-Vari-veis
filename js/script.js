f5d674f69186f833fadf86c5113c13bfd8dbdffe
Álvaro Costa Paulo <acptdb@gmail.com>
2021-03-28 17:42:17 -0300

    continue

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ let segundoValor = parseFloat(prompt("Digite o segundo valor:"));
 resultado = primeiroValor + segundoValor;
 document.write("<h1>" + primeiroValor.toFixed(2) + " + " + segundoValor.toFixed(2)
    + " = " + resultado + "</h1>");
+//let operacao = parseFloat(prompt("Digite 1 para fazer uma divisão, 2 para multiplicar, 3 para soma, 4 para subtração:"))
+let primeiroValor = parseFloat(prompt("Digite o primeiro valor:"));
+let segundoValor = parseFloat(prompt("Digite o segundo valor:"));
+resultado = primeiroValor + segundoValor;
+document.write("<h1>" + primeiroValor.toFixed(2) + " + " + segundoValor.toFixed(2)
+   + " = " + resultado + "</h1>");
